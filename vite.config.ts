0d66ab6e9e2c9f3e599ee0be2e8db773f15683bf
minhluanVlong <minhluan.ttytcl@gmail.com>
2026-04-11 20:15:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/Th-i-gian-Phun-Khi-Dung/',
       server: {
         port: 3000,
         host: '0.0.0.0',
